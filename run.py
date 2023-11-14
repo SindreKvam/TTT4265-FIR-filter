69ee4dc68dd6817de94b75db26f88f2b39fff91f
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2023-11-14 11:22:37 +0100

    Added ieee proposed library

--- run.py
+++ run.py
@@ -8,6 +8,12 @@ vu.add_com()
 smk_lib = vu.add_library("smk_lib")
 smk_lib.add_source_files("*/*.vhd")
 
+ieee_proposed_lib = vu.add_library("ieee_proposed")
+# ieee_proposed_lib.add_source_files("quartus/lib/vhdl/ieee_proposed/*.vhdl", vhdl_standard="1993")
+ieee_proposed_lib.add_source_file("quartus/lib/vhdl/ieee_proposed/math_utility_pkg.vhdl", vhdl_standard="2008")
+ieee_proposed_lib.add_source_file("quartus/lib/vhdl/ieee_proposed/fixed_float_types_c.vhdl", vhdl_standard="2008")
+ieee_proposed_lib.add_source_file("quartus/lib/vhdl/ieee_proposed/fixed_pkg_c.vhdl", vhdl_standard="1993")
+
 for tb in smk_lib.get_test_benches():
 
     # Load any wave.do files found in the testbench folders when running in GUI mode

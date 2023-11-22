409a183df32b8e75a52e40667c715a252342f94f
Sindre Mandelid Kvam <sindre.m.kvam@gmail.com>
2023-11-22 21:44:00 +0100

    Updated top test_bench

--- tb/top_tb.vhd
+++ tb/top_tb.vhd
@@ -23,7 +23,7 @@ architecture sim of top_tb is
     constant clk_period : time := 1 sec / clk_hz;
 
     -- Constants used to generate sine-wave
-    constant f : real := 2880.0;
+    constant f : real := 500.0;
     constant f_s : real := 8000.0;
     constant f_d : real := f / f_s;
     signal sine : std_logic_vector(0 downto 0);
@@ -32,8 +32,8 @@ architecture sim of top_tb is
     signal rst_n : std_logic := '0';
 
     -- LFSR
-    signal lfsr_rst_n : std_logic := '0';
     signal lfsr_ready : std_logic := '0';
+    signal lfsr_valid : std_logic := '0';
     signal d_valid : std_logic;
     signal data_lfsr : std_logic_vector(0 downto 0);
 
@@ -42,8 +42,10 @@ architecture sim of top_tb is
     signal fir_prev_ready : std_logic := '0';
     signal next_ready : std_logic;
     signal next_valid : std_logic;
-    signal data_out : sfixed(10 downto -13);
+    signal data_out : sfixed(0 downto -23);
     signal clk_8k : std_logic;
+
+    signal unsigned_data_out : unsigned(23 downto 0);
 
 begin
 
@@ -55,10 +57,10 @@ begin
     )
     port map (
         clk => clk,
-        rst_n => lfsr_rst_n,
+        rst_n => rst_n,
 
         ready => lfsr_ready,
-        --valid => d_valid,
+        valid => lfsr_valid,
         data => data_lfsr
     );
 
@@ -70,9 +72,9 @@ begin
         clk => clk,
         rst_n => rst_n,
 
-        prev_ready => fir_prev_ready,
-        prev_valid => d_valid,
-        data_in => fir_data_in,
+        prev_ready => lfsr_ready,
+        prev_valid => lfsr_valid,
+        data_in => data_lfsr, --fir_data_in,
 
         next_ready => next_ready,
         next_valid => next_valid,
@@ -91,6 +93,8 @@ begin
         clk_8k => clk_8k
     );
     
+    fir_data_in <= sine;
+    unsigned_data_out <= unsigned(resize(data_out + to_sfixed(0.7083 , data_out), data_out)); -- resize(  * 2, unsigned_data_out)
 
     SEQUENCER_PROC : process
     begin
@@ -105,20 +109,7 @@ begin
 
         rst_n <= '1';
 
-        -- Send sine wave in
-        --fir_prev_ready <= not fir_prev_ready after sine_period;
-        --fir_data_in <= not fir_data_in after sine_period;
-        
-        fir_data_in <= sine;
-
-        wait for clk_period * 1e6;
-
-        -- Send LFSR to FIR filter
-        --fir_prev_ready <= lfsr_ready;
-        --fir_data_in <= data_lfsr;
-        --lfsr_rst_n <=  '1';
-
-        wait for clk_period * 1e6;
+        wait for clk_period * 2e7;
 
         -------------------------------
         -- VUNIT cleanup
@@ -137,10 +128,10 @@ begin
         if rising_edge(clk) then
             v_sin := sin(MATH_2_PI * f_d * v_tstep);
             
-            d_valid <= '0';
+            --d_valid <= '0';
             if clk_8k = '1' then
                 v_tstep := v_tstep + 1.0;
-                d_valid <= '1';
+                --d_valid <= '1';
             end if;
             
 --
